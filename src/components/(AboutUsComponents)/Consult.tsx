e262637b6eb0a5466123468b273c779a023b10d1
KingZedPriest <Charleschukwuemeka47@gmail.com>
2023-11-24 07:14:21 +0100

    Fix: Add some missing components

--- src/components/(AboutUsComponents)/Consult.tsx
+++ src/components/(AboutUsComponents)/Consult.tsx
@@ -10,12 +10,12 @@ const Consult = () => {
     <main className="relative bg-[url('../../public/images/bg5.jpeg')] bg-cover bg-center object-center py-[8rem] text-white selection:bg-orange selection:text-white">
       <div className="absolute inset-0 bg-black bg-opacity-70"></div>
       <div className="relative px-4 sm:px-10 md:px-12 lg:px-14">
-        <div className="flex flex-col items-center justify-center w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] text-center mx-auto">
+        <div className="flex flex-col items-center justify-center w-[90%] sm:w-[80%] md:w-[70%] xl:w-[60%] text-center mx-auto">
           <Heading
             heading="Consult The Services You"
             coloredHeading="Need Now!!!"
           />
-          <p className="text-xs sm:text-sm md:text-base mt-10">
+          <p className="text-sm md:text-base mt-10 leading-relaxed">
             Discover a seamless solution to all your shipping and courier needs
             with United Traverse. Our comprehensive services ensure that whether
             it&apos;s express deliveries, international shipments, or custom
@@ -23,9 +23,23 @@ const Consult = () => {
             requirements, providing reliability, efficiency, and a commitment to
             exceed your expectations.
           </p>
+          <div className="flex flex-col gap-y-3 lg:gap-y-0 lg:flex-row lg:justify-between mt-10 w-full items-center">
+            <div className="flex items-center gap-x-1 w-[12rem]">
+              <MdGppGood className=" text-orange text-3xl"/>
+              <p className="font-semibold text-xs sm:text-sm md:text-base">Fast Delivery</p>
+            </div>
+            <div className="flex items-center gap-x-1 w-[12rem]">
+              <MdGppGood className=" text-orange text-3xl"/>
+              <p className="font-semibold text-xs sm:text-sm md:text-base">Competitive Prices</p>
+            </div>
+            <div className="flex items-center gap-x-1 w-[12rem]">
+              <MdGppGood className=" text-orange text-3xl"/>
+              <p className="font-semibold text-xs sm:text-sm md:text-base">Wide Delivery Area</p>
+            </div>
+          </div>
           <Link
             href="/contact"
-            className="mt-10 border border-orange bg-orange px-4 py-2 text-white duration-500 hover:bg-white hover:text-orange"
+            className="mt-20 border border-orange bg-orange px-6 py-2 text-white duration-500 hover:bg-white hover:text-orange"
           >
             Contact Us
           </Link>
