d6fd4c59c4e99f84de7cd66f1c90b13756586da7
KingZedPriest <charleschukwuemeka47@gmail.com>
2023-12-14 17:12:53 +0100

    Revert "Feat: Admin Dashboard page completed."
    This reverts commit 7e24ecb51f662687270d5fa1e2daae392074c90a.

--- src/app/(adminPages)/admin/dashboard/page.tsx
+++ src/app/(adminPages)/admin/dashboard/page.tsx
@@ -1,7 +1,6 @@
 //Import Needed Components
 import AdminHeading from "@/components/(AdminComponents)/AdminHeading";
 import Summary from "@/components/(AdminComponents)/Summary";
-import LastDelivery from "@/components/(AdminComponents)/Transaction";
 
 const page = () => {
     return ( 
@@ -9,9 +8,8 @@ const page = () => {
          <div className="px-4 py-4 lg:px-10">
             <AdminHeading route="home" coloredRoute="dashboard"/>
          </div>
-          <div className="bg-bgWhite px-4 py-6 lg:px-10 h-screen">
+          <div className="bg-bgWhite px-4 py-6 lg:px-10">
             <Summary />
-            <LastDelivery />
            </div> 
         </main>
      );
