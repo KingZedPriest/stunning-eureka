7e24ecb51f662687270d5fa1e2daae392074c90a
KingZedPriest <Charleschukwuemeka47@gmail.com>
2023-12-14 17:02:16 +0100

    Feat: Admin Dashboard page completed.

--- src/app/(adminPages)/admin/dashboard/page.tsx
+++ src/app/(adminPages)/admin/dashboard/page.tsx
@@ -1,6 +1,7 @@
 //Import Needed Components
 import AdminHeading from "@/components/(AdminComponents)/AdminHeading";
 import Summary from "@/components/(AdminComponents)/Summary";
+import LastDelivery from "@/components/(AdminComponents)/Transaction";
 
 const page = () => {
     return ( 
@@ -8,8 +9,9 @@ const page = () => {
          <div className="px-4 py-4 lg:px-10">
             <AdminHeading route="home" coloredRoute="dashboard"/>
          </div>
-          <div className="bg-bgWhite px-4 py-6 lg:px-10">
+          <div className="bg-bgWhite px-4 py-6 lg:px-10 h-screen">
             <Summary />
+            <LastDelivery />
            </div> 
         </main>
      );
---
+++ src/components/(AdminComponents)/Transaction.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+//Import Needed Icons
+import { PiPackageFill } from "react-icons/pi";
+
+const Transaction = () => {
+    return ( 
+        <main className="mt-10 overflow-x-auto special">
+            <div className="flex justify-between w-full min-w-[40rem]">
+                <p className="">Ongoing Shipments</p>
+                <p className="text-gray-500">Your Last 7 Shipments</p>
+            </div>
+            <div className="mt-10 w-full min-w-[40rem]">
+               <div className="flex justify-between items-center mt-4 border-b border-gray-400 py-2">
+                    <PiPackageFill className="text-orange" size={30}/>
+                    <p className="text-lg lg:text-xl font-bold">UT786901</p>
+                    <p className="text-lg lg:text-xl font-bold">Newyork</p>
+                    <p className="text-lg lg:text-xl font-bold">10, Aug 2023</p>
+                    <p className="text-sm lg:text-base">Completed</p>
+                </div> 
+                <div className="flex justify-between items-center mt-4 border-b border-gray-400 py-2">
+                    <PiPackageFill className="text-orange" size={30}/>
+                    <p className="text-lg lg:text-xl font-bold">UT786901</p>
+                    <p className="text-lg lg:text-xl font-bold">Newyork</p>
+                    <p className="text-lg lg:text-xl font-bold">10, Aug 2023</p>
+                    <p className="text-sm lg:text-base">Completed</p>
+                </div> 
+                <div className="flex justify-between items-center mt-4 border-b border-gray-400 py-2">
+                    <PiPackageFill className="text-orange" size={30}/>
+                    <p className="text-lg lg:text-xl font-bold">UT786901</p>
+                    <p className="text-lg lg:text-xl font-bold">Newyork</p>
+                    <p className="text-lg lg:text-xl font-bold">10, Aug 2023</p>
+                    <p className="text-sm lg:text-base">Completed</p>
+                </div> 
+                <div className="flex justify-between items-center mt-4 border-b border-gray-400 py-2">
+                    <PiPackageFill className="text-orange" size={30}/>
+                    <p className="text-lg lg:text-xl font-bold">UT786901</p>
+                    <p className="text-lg lg:text-xl font-bold">Newyork</p>
+                    <p className="text-lg lg:text-xl font-bold">10, Aug 2023</p>
+                    <p className="text-sm lg:text-base">Completed</p>
+                </div> 
+                <div className="flex justify-between items-center mt-4 border-b border-gray-400 py-2">
+                    <PiPackageFill className="text-orange" size={30}/>
+                    <p className="text-lg lg:text-xl font-bold">UT786901</p>
+                    <p className="text-lg lg:text-xl font-bold">Newyork</p>
+                    <p className="text-lg lg:text-xl font-bold">10, Aug 2023</p>
+                    <p className="text-sm lg:text-base">Completed</p>
+                </div> 
+                <div className="flex justify-between items-center mt-4 border-b border-gray-400 py-2">
+                    <PiPackageFill className="text-orange" size={30}/>
+                    <p className="text-lg lg:text-xl font-bold">UT786901</p>
+                    <p className="text-lg lg:text-xl font-bold">Newyork</p>
+                    <p className="text-lg lg:text-xl font-bold">10, Aug 2023</p>
+                    <p className="text-sm lg:text-base">Completed</p>
+                </div> 
+                <div className="flex justify-between items-center mt-4 border-b border-gray-400 py-2">
+                    <PiPackageFill className="text-orange" size={30}/>
+                    <p className="text-lg lg:text-xl font-bold">UT786901</p>
+                    <p className="text-lg lg:text-xl font-bold">Newyork</p>
+                    <p className="text-lg lg:text-xl font-bold">10, Aug 2023</p>
+                    <p className="text-sm lg:text-base">Completed</p>
+                </div> 
+            </div>
+            <div className="flex justify-end my-10">
+                <Link href="/admin/orders" className="p-3 bg-orange text-white rounded-md font-semibold hover:bg-orange1 duration-500">Create New Order</Link>
+            </div>
+        </main>
+     );
+}
+ 
+export default Transaction;
