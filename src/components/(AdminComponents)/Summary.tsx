a6fc1501528c744a1f4a39db3d815fe01ef8242c
KingZedPriest <charleschukwuemeka47@gmail.com>
2023-12-14 13:56:14 +0100

    Feat: Finish the summary part of the code

--- src/components/(AdminComponents)/Summary.tsx
+++ src/components/(AdminComponents)/Summary.tsx
@@ -1,5 +1,10 @@
 //Import Needed Utilities
 import { formatDate } from "@/lib/dateUtils";
+//Import Needed Icons
+import { PiPackageFill } from "react-icons/pi";
+import { TbProgressDown } from "react-icons/tb";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+
 const Summary = () => {
     //For the Date
     const currentDate = new Date()
@@ -10,15 +15,27 @@ const Summary = () => {
                <p className="text-xs sm:text-sm lg:text-base text-gray-600 font-semibold">{formattedDate}.</p> 
             </div>
             <div className="flex gap-x-5 overflow-x-auto special mt-10 pb-4">
-               <div className="w-1/4 min-w-[18rem] bg-[#4CB9E7] h-40 rounded-md">
-
-            </div>
-            <div className="w-1/4 min-w-[18rem] bg-[#FB8B24] h-40 rounded-md">
-
-            </div>
-            <div className="w-1/4 min-w-[18rem] bg-[#65B741] h-40 rounded-md">
-
-            </div> 
+               <div className="w-1/4 min-w-[18rem] bg-[#4CB9E7] p-8 flex gap-x-5 rounded-md">
+               <PiPackageFill className="text-white" size={40}/>
+                    <div className="text-white ">
+                        <p className="text-2xl md:text-3xl font-bold">0</p>
+                        <p className="text-base">Total Packages</p>
+                    </div>
+                </div>
+                <div className="w-1/4 min-w-[18rem] bg-[#FB8B24] p-8 flex gap-x-5 rounded-md">
+                    <TbProgressDown className="text-white" size={40}/>
+                    <div className="text-white ">
+                        <p className="text-2xl md:text-3xl font-bold">0</p>
+                        <p className="text-base">Packages In Progress</p>
+                    </div>
+                </div>
+                <div className="w-1/4 min-w-[18rem] bg-[#65B741] p-8 flex gap-x-5 rounded-md">
+                    <BsFillPatchCheckFill className="text-white" size={40}/>
+                    <div className="text-white ">
+                        <p className="text-2xl md:text-3xl font-bold">0</p>
+                        <p className="text-base">Total Packages Delivered</p>
+                    </div>
+                </div> 
             </div>
             
         </main>
