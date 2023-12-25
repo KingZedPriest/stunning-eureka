30487a9bd655490472286b71bf3744033bfdfe66
KingZedPriest <charleschukwuemeka47@gmail.com>
2023-12-25 20:27:09 +0100

    Feat: Add the Tracking page for the other side

--- src/app/(clientPages)/tracking/page.tsx
+++ src/app/(clientPages)/tracking/page.tsx
@@ -1,7 +1,16 @@
+//Import Needed Components
+import Consult from "@/components/(AboutUsComponents)/Consult";
+import CTA from "@/components/(LandingPageComponents)/CTA";
+import TrackingForm from "@/components/(LandingPageComponents)/TrackingForm";
 const page = () => {
     return ( 
-        <main>
-            <h1>Tracking</h1>
+        <main className="mt-10">
+            <div className="mb-10">
+                <TrackingForm />
+            </div>
+            
+            <Consult />
+            <CTA />
         </main>
      );
 }
